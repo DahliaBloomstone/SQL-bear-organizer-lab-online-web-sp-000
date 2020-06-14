c7ca07a3a71520d984ca07465efb1411dc5c322b
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-14 22:00:35 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,36 +3,36 @@ def selects_all_female_bears_return_name_and_age
 end	end
 
 
-def selects_all_bears_names_and_orders_in_alphabetical_order	def selects_all_bears_names_and_orders_in_alphabetical_order
+def selects_all_bears_names_and_orders_in_alphabetical_order	
 "SELECT name FROM bears ORDER BY name"
 end	end
 
 
-def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest	def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
+def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest	
   "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;"
 end	end
 
 
-def selects_oldest_bear_and_returns_name_and_age	def selects_oldest_bear_and_returns_name_and_age
+def selects_oldest_bear_and_returns_name_and_age	
  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
 end	end
 
 
-def select_youngest_bear_and_returns_name_and_age	def select_youngest_bear_and_returns_name_and_age
+def select_youngest_bear_and_returns_name_and_age	
   "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;"
 end	end
 
 
-def selects_most_prominent_color_and_returns_with_count	def selects_most_prominent_color_and_returns_with_count
+def selects_most_prominent_color_and_returns_with_count	
  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1;"
 end	end
 
 
-def counts_number_of_bears_with_goofy_temperaments	def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"	  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy';"
+def counts_number_of_bears_with_goofy_temperaments	
+	  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy';"
 end	end
 
 
-def selects_bear_that_killed_Tim	def selects_bear_that_killed_Tim
-  "Write your SQL query here"	  "SELECT * FROM bears WHERE name IS NULL;"
+def selects_bear_that_killed_Tim	
+    "SELECT * FROM bears WHERE name IS NULL;"
 end	end
